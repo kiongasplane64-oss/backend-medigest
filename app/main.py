001097a9fe92bc7d7724cef0d8268f83be520190
Kionga Splane <kiongasplane64@gmail.com>
2026-02-24 03:41:50 +0100

    Add main

--- app/main.py
+++ app/main.py
@@ -103,7 +103,3 @@ app.include_router(
     prefix="/api/v1",
     tags=["Products"]
 )
-
-if __name__=="__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
